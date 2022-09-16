99153ba744bae7bf9b56c48c5cccfe07ee285656
Fabio Florencio <fabioflorencioxyz@gmail.com>
2022-09-15 22:16:36 -0300

    terceiro commit

---
+++ ExForEach/foreachListaArq.ps1
@@ -0,0 +1,23 @@
+﻿     
+
+#Listando arquivos de acordo com o tamanho
+
+#$env:SystemRoot\system32 -> Envoca o caminho abaixo
+
+
+
+foreach($arq in gci C:\WINDOWS\System32 -File){
+    if($arq.length -gt 1mb){$arq.Name}    
+}
+
+
+
+
+
+
+
+
+
+
+
+
